ab5c7e1402bf0b3c79cdce9185e067a22920795c
Thomas Stussi <tstussi06@gmail.com>
2020-07-27 12:00:25 -0700

    initial js

---
+++ board files/board.js
@@ -0,0 +1,7 @@
+import { setPiece, clearBoard, redStartingPositions, blackStartingPositions } from './board-utils.js';
+
+const allClickableSquares = document.querySelectorAll('.clickable');
+
+console.log(allClickableSquares);
+
+//setPiece();
